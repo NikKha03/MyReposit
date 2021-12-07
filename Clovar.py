2af21884d225ca8d2faf62d86ceabe74638a3915
NikKha03 <kolyakhalimendik@gmail.com>
2021-12-07 12:19:56 +0300

    j

---
+++ Clovar.py
@@ -0,0 +1,9 @@
+Capitals = dict()
+Capitals['Apple'] = 'Яблоко'
+Capitals['Orange'] = 'Апельсин'
+Capitals['Pear'] = 'Груша'
+print('Введите слово:')
+a = input()
+Capitals
+if a in Capitals:
+    print('Перевод:', Capitals[a])
